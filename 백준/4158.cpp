6eec20ba75ffc0135c6f0f4cb5c02f1180491417
jjh <wognsddd777@gmail.com>
2025-05-13 23:22:57 +0900

    통과된 코드

--- 백준/4158.cpp
+++ 백준/4158.cpp
@@ -1,49 +1,32 @@
-#include <cstdlib>
-#include <iostream>
+#include <cstdio>
 #include <vector>
 
-using namespace std;
-
-int main()
-{
-    while(true)
-    {
-
+int main() {
+    while (true) {
         int N, M;
-        int cnt = 0;
-    
-        cin >> N >> M;
+        if (scanf("%d %d", &N, &M) != 2) break;
+        if (N == 0 && M == 0) break;
 
-        if(N == 0 && M == 0)
-            break;
-    
-        vector<int> Ns(N), Ms(M);
+        std::vector<int> Ns(N), Ms(M);
+        for (int i = 0; i < N; ++i)
+            scanf("%d", &Ns[i]);
+        for (int i = 0; i < M; ++i)
+            scanf("%d", &Ms[i]);
 
-        for(int i = 0; i < N; i++)
-            cin >> Ns[i];
-        for(int i = 0; i < M; i++)
-            cin >> Ms[i];
-
-        int i = 0, j = 0;
-        while(i < N && j < M)
-        {
-            if(Ns[i] == Ms[j])
-            {
+        int i = 0, j = 0, cnt = 0;
+        while (i < N && j < M) {
+            if (Ns[i] == Ms[j]) {
                 cnt++;
                 i++;
                 j++;
-            }
-            else if(Ns[i] < Ms[j])
-            {
+            } else if (Ns[i] < Ms[j]) {
                 i++;
-            }
-            else
-            {
+            } else {
                 j++;
             }
         }
-        cout << cnt << endl;
-    }
 
-    return EXIT_SUCCESS;
+        printf("%d\n", cnt);
+    }
+    return 0;
 }
