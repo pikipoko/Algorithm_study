087906a75f769724be02ad9aa8284f71cf664504
정재훈 <wognsddd777@gmail.com>
2026-06-16 14:24:22 +0900

    over 1 hour

---
+++ java/src/programmers/단어 변환/Solution.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution("hit", "cog", new String[]{"hot", "dot", "dog", "lot", "log", "cog"}));
+        System.out.println(sol.solution("hit", "cog", new String[]{"hot", "dot", "dog", "lot", "log"}));
+    }
+
+    int minCnt;
+
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+
+        boolean canBeChanged = false;
+        for(String w : words)
+        {
+            if(w.equals(target))
+            {
+                canBeChanged = true;
+                break;
+            }
+        }
+        if(!canBeChanged)
+            return 0;
+
+        minCnt = words.length+1;
+
+        boolean[] visited = new boolean[words.length];
+        dfs(words, visited, begin, target, 0);
+        return minCnt;
+    }
+
+    private void dfs(String[] words, boolean[] visited, String cur, String target, int cnt)
+    {
+        if(minCnt <= cnt)
+            return;
+        if(cur.equals(target))
+        {
+            minCnt = cnt;
+            return;
+        }
+
+        for(int i = 0; i < words.length; i++)
+        {
+            if(!visited[i] && isOneLetterDif(words[i], cur))
+            {
+                visited[i] = true;
+                dfs(words, visited, words[i], target, cnt+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    private boolean isOneLetterDif(String str1, String str2)
+    {
+        int cnt = 0;
+
+        for(int i = 0; i < str1.length(); i++)
+        {
+            if(str1.charAt(i) != str2.charAt(i))
+                cnt++;
+        }
+
+        return cnt == 1;
+    }
+
+}
