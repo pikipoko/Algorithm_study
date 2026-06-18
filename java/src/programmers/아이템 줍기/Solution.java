6638975df1594bf35bcbbd658d2c64c51951f77f
정재훈 <wognsddd777@gmail.com>
2026-06-18 10:45:34 +0900

    over 3 hours

--- java/src/programmers/아이템 줍기/Solution.java
+++ java/src/programmers/아이템 줍기/Solution.java
@@ -17,8 +17,8 @@ class Solution {
         Queue<int[]> q = new LinkedList<>();
 
         int[] maxXY = getMaxXY(rectangles);
-        int[][] visited = new int[maxXY[0]][maxXY[1]];
-        for(int[] v : visited) Arrays.fill(v, 0);
+        int[][] visited = new int[maxXY[0]+1][maxXY[1]+1];
+        for(int[] v : visited) Arrays.fill(v, -1);
 
         visited[start[0]][start[1]] = 0;
 
@@ -34,7 +34,7 @@ class Solution {
             {
                 int[] next = {cur[0]+dirs[i][0], cur[1]+dirs[i][1]};
 
-                if(next[0] >= 0 && next[1] >= 0 && next[0] <= maxXY[0] && next[1] <= maxXY[1] && cntCanMove(rectangles, next) > 0 && visited[next[0]][next[1]] == 0)
+                if(next[0] >= 0 && next[1] >= 0 && next[0] <= maxXY[0] && next[1] <= maxXY[1] && cntCanMove(rectangles, next, cur) > 0 && visited[next[0]][next[1]] == -1)
                 {
                     visited[next[0]][next[1]] = visited[cur[0]][cur[1]]+1;
                     q.add(next);
@@ -42,6 +42,8 @@ class Solution {
             }
 
         }
+
+        printCost(visited, maxXY);
 
         return visited[target[0]][target[1]];
     }
@@ -61,25 +63,24 @@ class Solution {
 
     }
 
-    private int cntCanMove(int[][] rectangles, int[] next)
+    private int cntCanMove(int[][] rectangles, int[] next, int[] cur)
     {
+        /*
+        현재와 다음 점이 같은 직사각형 위에 있는 경우만 갈 수 있음.
+         */
         int cntBoundary = 0;
         for(int i = 0; i < rectangles.length; i++)
         {
-            if(isBoundary(rectangles[i], next))
+            if(isRectangleBoundaryLine(rectangles[i], cur, next))
             {
                 cntBoundary++;
                 for(int j = 0; j < rectangles.length; j++)
                 {
                     if(i != j)
                     {
-                        if(isInside(rectangles[j], next))
+                        if(isRectangleIncludeLine(rectangles[j], cur, next))
                         {
                             return 0;
-                        }
-                        if(isBoundary(rectangles[j], next))
-                        {
-                            cntBoundary++;
                         }
                     }
                 }
@@ -88,38 +89,104 @@ class Solution {
         return cntBoundary;
     }
 
-    private boolean isBoundary(int[] rectangle, int[] cur)
+    private boolean isRectangleBoundaryLine(int[] rectangle, int[] cur, int[] next)
     {
-        // 위
-        if(cur[1] == rectangle[3] && cur[0] >= rectangle[0] && cur[0] <= rectangle[2])
-            return true;
+        int leftX   = rectangle[0];
+        int downY   = rectangle[1];
+        int rightX  = rectangle[2];
+        int upY     = rectangle[3];
 
-        // 아래
-        if(cur[1] == rectangle[1] && cur[0] >= rectangle[0] && cur[0] <= rectangle[2])
-            return true;
+        // x same, |
+        if(cur[0] == next[0])
+        {
+            // 왼쪽, 오른쪽 선
+            if(cur[0] == leftX || cur[0] == rightX)
+            {
+                if(downY <= cur[1] && cur[1] <= upY //
+                        && downY <= next[1] && next[1] <= upY)
+                {
+                    return true;
+                }
+            }
+        }
 
-        // 왼쪽
-        if(cur[0] == rectangle[0] && cur[1] >= rectangle[1] && cur[1] <= rectangle[3])
-            return true;
-
-        // 오른쪽
-        if(cur[0] == rectangle[2] && cur[1] >= rectangle[1] && cur[1] <= rectangle[3])
-            return true;
+        // y same, -
+        else
+        {
+            // 위, 아래 선
+            if(cur[1] == downY || cur[1] == upY)
+            {
+                if(leftX <= cur[0] && cur[0] <= rightX //
+                        && leftX <= next[0] && next[0] <= rightX)
+                {
+                    return true;
+                }
+            }
+        }
 
         return false;
     }
 
-    private boolean isInside(int[] rectangle, int[] cur)
+    private boolean isRectangleIncludeLine(int[] rectangle, int[] cur, int[] next)
     {
-        if(cur[0] > rectangle[0] && cur[0] < rectangle[2] && cur[1] > rectangle[1] && cur[1] < rectangle[3])
-            return true;
+        int leftX   = rectangle[0];
+        int downY   = rectangle[1];
+        int rightX  = rectangle[2];
+        int upY     = rectangle[3];
+
+        // x same, |
+        if(cur[0] == next[0])
+        {
+            if(leftX <= cur[0] && cur[0] <= rightX)
+            {
+                if(downY <= cur[1] && cur[1] <= upY //
+                        && downY <= next[1] && next[1] <= upY)
+                {
+                    return true;
+                }
+            }
+        }
+        // y same, -
+        else
+        {
+            if(downY <= cur[1] && cur[1] <= upY)
+            {
+                if(leftX <= cur[0] && cur[0] <= rightX //
+                        && leftX <= next[0] && next[0] <= rightX)
+                {
+                    return true;
+                }
+            }
+        }
 
         return false;
+    }
+
+    private void printCost(int[][] visited, int[] maxXY)
+    {
+        System.out.println("----".repeat(maxXY[0]));
+        for(int j = 0; j < maxXY[1]+1; j++)
+        {
+            for(int i = 0; i < maxXY[0]+1; i++)
+            {
+                int cur = visited[i][maxXY[1] - j];
+                if(cur != -1)
+                    System.out.printf("%2d ", visited[i][maxXY[1] - j]);
+                else
+                    System.out.print("   ");
+            }
+            System.out.println();
+        }
+        System.out.println("----".repeat(maxXY[0]));
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
 //        System.out.println(sol.solution(new String[]{"SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"}));
         System.out.println(sol.solution(new int[][]{{1,1,7,4}, {3,2,5,5}, {4,3,6,9}, {2,6,8,8}}, 1, 3, 7, 8));
+        System.out.println(sol.solution(new int[][]{{1,1,8,4}, {2,2,4,9}, {3,6,9,8}, {6,3,7,7}}, 9, 7, 6, 1));
+        System.out.println(sol.solution(new int[][]{{1,1,5,7}}, 1, 1, 4, 7));
+        System.out.println(sol.solution(new int[][]{{2,1,7,5}, {6,4,10,10}}, 3, 1, 7, 10));
+        System.out.println(sol.solution(new int[][]{{2,2,5,5}, {1,3,6,4}, {3,1,4,6}}, 1, 4, 6, 3));
     }
 }
